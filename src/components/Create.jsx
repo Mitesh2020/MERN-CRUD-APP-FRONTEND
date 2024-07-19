105388004a2b0a2651dc5dc7434c9b4c6e41674c
Mitesh Rathod <75556442+Mitesh2020@users.noreply.github.com>
2024-07-19 16:01:16 +0530

    Update Create.jsx

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -11,7 +11,7 @@ const Create = () => {
     e.preventDefault();
 
     const addUser = { name, email, age };
-    const response = await fetch("http://localhost:5000/api/user", {
+    const response = await fetch("https://mern-crud-app-backend-kohl.vercel.app/", {
       method: "POST",
       body: JSON.stringify(addUser),
       headers: {
