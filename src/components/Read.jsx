29baf2785c4d295c7276c53be0dc215f52eadf04
Mitesh2020 <miteshofficial007@gmail.com>
2024-07-19 11:54:29 +0530

    Added frontend

---
+++ src/components/Read.jsx
@@ -0,0 +1,81 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Read = () => {
+  const [data, setData] = useState();
+  const [error, setError] = useState("");
+
+  async function getData() {
+    const response = await fetch("http://localhost:5000/api/user");
+    const result = await response.json();
+
+    if (!response.ok) {
+      console.log(result.error);
+      setError(result.error);
+    }
+
+    if (response.ok) {
+      setData(result);
+    }
+  }
+
+  const handleDelete = async (id) => {
+    const response = await fetch(`http://localhost:5000/api/user/${id}`, {
+      method: "DELETE",
+    });
+
+    const result = await response.json();
+
+    if (!response.ok) {
+      console.log(result.error);
+      setError(result.error);
+    }
+
+    if (response.ok) {
+      setError("Deleted Successfully");
+      setTimeout(() => {
+        setError("");
+        getData();
+      }, 2000);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  console.log(data);
+
+  return (
+    <div className="container my-2">
+      {error && <div class="alert alert-danger">{error}</div>}
+      <h2 className="text-center">ALL DATA</h2>
+      <div className="row">
+        {data?.map((ele) => (
+          <div key={ele._id} className="col-3">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">{ele.name}</h5>
+                <h6 className="card-subtitle mb-1 text-muted">{ele.email}</h6>
+                <p className="card-text">{ele.age}</p>
+                <a
+                  href="#"
+                  class="card-link"
+                  onClick={() => handleDelete(ele._id)}
+                >
+                  DELETE
+                </a>
+                <Link to={`/${ele._id}`} class="card-link">
+                  UPDATE
+                </Link>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Read;
---
+++ src/components/Update.jsx
@@ -0,0 +1,102 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+const Update = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [age, setAge] = useState(0);
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  const getSingleUser = async () => {
+    const response = await fetch(`http://localhost:5000/api/user/${id}`);
+    const result = await response.json();
+
+    if (!response.ok) {
+      console.log(result.error);
+      setError(result.error);
+    }
+
+    if (response.ok) {
+      setError("");
+      console.log("updated user : ", result);
+      setName(result.name);
+      setEmail(result.email);
+      setAge(result.age);
+    }
+  };
+
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+
+    const updateUser = { name, email, age };
+    const response = await fetch(`http://localhost:5000/api/user/${id}`, {
+      method: "PUT",
+      body: JSON.stringify(updateUser),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const result = await response.json();
+
+    if (!response.ok) {
+      console.log(result.error);
+      setError(result.error);
+    }
+
+    if (response.ok) {
+      setError("");
+      navigate("/all");
+    }
+  };
+
+  useEffect(() => {
+    getSingleUser();
+  }, []);
+
+  return (
+    <div className="container my-2">
+      {error && <div class="alert alert-danger">{error}</div>}
+      <h2 className="text-center">UPDATE THE DATA</h2>
+
+      <form onSubmit={handleUpdate}>
+        <div className="mb-3">
+          <label className="form-label">NAME</label>
+          <input
+            type="text"
+            className="form-control"
+            value={name}
+            required="true"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">EMAIL</label>
+          <input
+            type="email"
+            className="form-control"
+            value={email}
+            required="true"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">AGE</label>
+          <input
+            type="number"
+            className="form-control"
+            value={age}
+            onChange={(e) => setAge(e.target.value)}
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Update;
